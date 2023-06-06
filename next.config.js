a03354edb0e5b2721708bb4a5876c6896dd812d9
nicolas-source <nzheng8@student.ubc.ca>
2023-06-05 17:55:22 -0700

    first push using github actions

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
+//
+// module.exports = nextConfig
 
-module.exports = nextConfig
+
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+    assetPrefix: isProd ? '/your-github-repo-name/' : '',
+    images: {
+        unoptimized: true,
+    },
+}
