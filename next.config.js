4c865f80a93e696fb29ebbcc2ea91d58bf388262
nicolas-source <nzheng8@student.ubc.ca>
2023-06-06 18:28:29 -0700

    updated next.config.js for local dev

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 // https://github.com/gregrickaby/nextjs-github-pages
+// nextConfig = {} for local dev
 
 /**
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-    output: 'export',
-    basePath: '/nicolas',
-    experimental: {
-        appDir: true,
-    },
+    // output: 'export',
+    // basePath: '/nicolas',
+    // experimental: {
+    //     appDir: true,
+    // },
 }
 
 module.exports = nextConfig
