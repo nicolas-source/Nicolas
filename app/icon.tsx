754a7897d99c0791fe8ff78fc9277acc93aba619
nicolas-source <nzheng8@student.ubc.ca>
2023-06-15 10:05:41 -0700

    Formatting completed

--- app/icon.tsx
+++ app/icon.tsx
@@ -18,7 +18,7 @@ export default function Icon() {
             <div
                 style={{
                     fontSize: 24,
-                    background: 'white',
+                    // background: 'white',
                     width: '100%',
                     height: '100%',
                     display: 'flex',
