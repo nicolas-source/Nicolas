ee710154388e3c97b96fdbc21a14f83ec7d9a517
nicolas-source <nzheng8@student.ubc.ca>
2023-07-16 23:10:09 -0700

    figured out mapping images

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,7 +8,6 @@ import projectData from '@/data/projectData'
 import internal from "stream";
 
 export default function Projects() {
-    console.log(projectData)
     return (
         <section>
             <div className="max-w-6xl mx-auto px-4 sm:px-6">
@@ -24,115 +23,52 @@ export default function Projects() {
                     <div className="max-w-sm mx-auto grid gap-8 lg:grid-cols-2 lg:gap-6 items-start lg:max-w-none"
                          data-aos="fade-up" data-aos-delay="200">
 
-                        <div className="max-w-md mx-auto bg-white rounded-xl shadow-lg overflow-hidden md:max-w-2xl">
-                            <div className="md:flex">
-                                <div className="md:shrink-0">
-                                    <Image className="h-48 w-full object-cover md:h-full md:w-48"
-                                           src={TestimonialImage01}
-                                           alt="Modern building architecture"/>
-                                </div>
-                                <div className="p-8">
-                                    {/*<div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>*/}
-                                    <Link href="/projects/ProteinPrediction"
-                                          className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
-                                        Machine Learning (NLP) for Protein Prediction
-                                    </Link>
-
-                                    <p className="mt-2 text-slate-500">Looking to take your team away on a retreat to
-                                        enjoy awesome food
-                                        and take in some sunshine? We have a list of places to do just that.</p>
-                                </div>
-                            </div>
-                        </div>
-
-
                         {
                             projectData.map((project, index) => (
-                                    <div
+                                    <div key={project.id}
                                         className="max-w-md mx-auto bg-white rounded-xl shadow-lg overflow-hidden md:max-w-2xl">
                                         <div className="md:flex">
                                             <div className="md:shrink-0">
                                                 <Image className="h-48 w-full object-cover md:h-full md:w-48"
-                                                       src={TestimonialImage01}
-                                                       alt="Modern building architecture"/>
+                                                       src="/images/CapstonePoster.png"
+                                                       // src={TestimonialImage01}
+                                                       width={500}
+                                                       height={500}
+                                                       alt="Project Image"/>
                                             </div>
-                                            <div className="p-8">
-                                                <Link href="#"
+                                            <div className="px-8 py-6">
+                                                <Link href={project.link}
                                                       className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
                                                     {project.name}
                                                 </Link>
                                                 <ul className="list-disc">
                                                     {
                                                         project.description.map((item, index) => (
-                                                                <li className="mt-1 text-slate-700">
+                                                                <li key={project.id}
+                                                                    className="mt-1 text-slate-700">
                                                                     {item}
                                                                 </li>
                                                             )
                                                         )
                                                     }
                                                 </ul>
+                                                {
+                                                    project.tools.map((item, index) => (
+                                                            <span key={project.id}
+                                                                className="inline-flex items-center rounded-full bg-gray-50 px-2 py-1 mx-1 mt-3
+                                                                text-sm uppercase font-semibold text-green-600
+                                                                ring-1 ring-inset ring-green-500 ring-2">
+                                                                {item}
+                                                            </span>
+                                                        )
+                                                    )
+                                                }
                                             </div>
                                         </div>
                                     </div>
                                 )
                             )
                         }
-                        <div className="max-w-md mx-auto bg-white rounded-xl shadow-lg overflow-hidden md:max-w-2xl">
-                            <div className="md:flex">
-                                <div className="md:shrink-0">
-                                    <Image className="h-48 w-full object-cover md:h-full md:w-48"
-                                           src={TestimonialImage01}
-                                           alt="Modern building architecture"/>
-                                </div>
-                                <div className="p-8">
-                                    {/*<div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>*/}
-                                    <Link href="#"
-                                          className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible
-                                        accommodation for your team</Link>
-                                    <p className="mt-2 text-slate-500">Looking to take your team away on a retreat to
-                                        enjoy awesome food
-                                        and take in some sunshine? We have a list of places to do just that.</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="max-w-md mx-auto bg-white rounded-xl shadow-lg overflow-hidden md:max-w-2xl">
-                            <div className="md:flex">
-                                <div className="md:shrink-0">
-                                    <Image className="h-48 w-full object-cover md:h-full md:w-48"
-                                           src={TestimonialImage01}
-                                           alt="Modern building architecture"/>
-                                </div>
-                                <div className="p-8">
-                                    {/*<div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>*/}
-                                    <Link href="#"
-                                          className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible
-                                        accommodation for your team</Link>
-                                    <p className="mt-2 text-slate-500">Looking to take your team away on a retreat to
-                                        enjoy awesome food
-                                        and take in some sunshine? We have a list of places to do just that.</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="max-w-md mx-auto bg-white rounded-xl shadow-lg overflow-hidden md:max-w-2xl">
-                            <div className="md:flex">
-                                <div className="md:shrink-0">
-                                    <Image className="h-48 w-full object-cover md:h-full md:w-48"
-                                           src={TestimonialImage01}
-                                           alt="Modern building architecture"/>
-                                </div>
-                                <div className="p-8">
-                                    {/*<div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>*/}
-                                    <Link href="#"
-                                          className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible
-                                        accommodation for your team</Link>
-                                    <p className="mt-2 text-slate-500">Looking to take your team away on a retreat to
-                                        enjoy awesome food
-                                        and take in some sunshine? We have a list of places to do just that.</p>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
--- data/projectData.js
+++ data/projectData.js
@@ -3,24 +3,40 @@ export default [
         id: 1,
         name: "Machine Learning (NLP) for Protein Prediction",
         description: ["Looking to take your team away on a retreat to enjoy","awesome food and take in some sunshine? We have a list of places to do just that."],
-        image: "@/public/images/CapstonePoster.png"
+        image: "/images/CapstonePoster.png",
+        link: "/projects/ProteinPrediction",
+        tools: ["React", "NextJs"]
     },
     {
         id: 2,
         name: "Machine Learning (NLP) for Protein Prediction",
         description: ["Looking to take your team away on a retreat to enjoy","awesome food and take in some sunshine? We have a list of places to do just that."],
-        image: "@/public/images/CapstonePoster.png"
+        image: "/images/CapstonePoster.png",
+        link: "/projects/ProteinPrediction",
+        tools: ["React", "NextJs"]
     },
     {
         id: 3,
         name: "Machine Learning (NLP) for Protein Prediction",
         description: ["Looking to take your team away on a retreat to enjoy","awesome food and take in some sunshine? We have a list of places to do just that."],
-        image: "@/public/images/CapstonePoster.png"
+        image: "/images/CapstonePoster.png",
+        link: "/projects/ProteinPrediction",
+        tools: ["React", "NextJs"]
     },
     {
         id: 4,
         name: "Machine Learning (NLP) for Protein Prediction",
         description: ["Looking to take your team away on a retreat to enjoy","awesome food and take in some sunshine? We have a list of places to do just that."],
-        image: "@/public/images/CapstonePoster.png"
+        image: "/images/CapstonePoster.png",
+        link: "/projects/ProteinPrediction",
+        tools: ["React", "NextJs"]
+    },
+    {
+        id: 5,
+        name: "Machine Learning (NLP) for Protein Prediction",
+        description: ["Looking to take your team away on a retreat to enjoy","awesome food and take in some sunshine? We have a list of places to do just that."],
+        image: "/images/CapstonePoster.png",
+        link: "/projects/ProteinPrediction",
+        tools: ["React", "NextJs"]
     }
 ]
