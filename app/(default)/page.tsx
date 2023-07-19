cefbbb2b651c0c752743bb13f2650e427778284a
nicolas-source <nzheng8@student.ubc.ca>
2023-07-19 15:16:24 -0700

    updated images, added project component

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -4,20 +4,17 @@ export const metadata = {
 }
 
 import Hero from '@/components/hero'
-import Features from '@/components/features'
-import Zigzag from '@/components/zigzag'
 import ExperienceList from "@/components/ExperienceList";
 import EducationList from "@/components/EducationList";
+import Projects from "@/components/Projects";
 
 export default function Home() {
     return (
         <>
             <Hero/>
+            <Projects/>
             <EducationList/>
             <ExperienceList/>
-            {/*<Features />*/}
-            <Zigzag/>
-
         </>
     )
 }
