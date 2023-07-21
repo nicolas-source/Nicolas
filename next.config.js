4308041a4e4735aaf51afefc3ea7678db787c938
nicolas-source <nzheng8@student.ubc.ca>
2023-07-20 19:12:24 -0700

    m

--- next.config.js
+++ next.config.js
@@ -10,11 +10,11 @@ const assetPrefix = `/${repo}/`;
 const basePath = `/${repo}`;
 
 const nextConfig = {
-    // trailingSlash: true,
+    trailingSlash: true,
 
     output: "export",
-    // basePath: basePath,
-    // assetPrefix: assetPrefix,
+    basePath: basePath,
+    assetPrefix: assetPrefix,
     images: {
         unoptimized: true,
     },
