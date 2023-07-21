79c92a8d707f22be73591d0cfca5e7135919adbb
nicolas-source <nzheng8@student.ubc.ca>
2023-07-20 18:04:08 -0700

    m on config

--- next.config.js
+++ next.config.js
@@ -5,30 +5,30 @@
  * @type {import('next').NextConfig}
  **/
 
-const repo = "nicolas";
-const assetPrefix = `/${repo}/`;
-const basePath = `/${repo}`;
-
-const nextConfig = {
-    trailingSlash: true,
-
-    output: "export",
-    basePath: basePath,
-    assetPrefix: assetPrefix,
-    images: {
-        unoptimized: true,
-    },
-};
+// const repo = "nicolas";
+// const assetPrefix = `/${repo}/`;
+// const basePath = `/${repo}`;
+//
+// const nextConfig = {
+//     trailingSlash: true,
+//
+//     output: "export",
+//     basePath: basePath,
+//     assetPrefix: assetPrefix,
+//     images: {
+//         unoptimized: true,
+//     },
+// };
 
 /**
  * @type {import('next').NextConfig}
  */
-// const nextConfig = {
-//     // output: 'export',
-//     // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-//     // trailingSlash: true,
-//     // Optional: Change the output directory `out` -> `dist`
-//     // distDir: 'dist',
-// }
+const nextConfig = {
+    // output: 'export',
+    // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+    // trailingSlash: true,
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+}
 
 module.exports = nextConfig
